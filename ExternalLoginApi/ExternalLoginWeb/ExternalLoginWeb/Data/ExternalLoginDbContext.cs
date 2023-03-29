6d7a3c13921b2e8ab1fdaeb63bb2d5e90a6df8ca
Nouf283 <noufawal0311@gmail.com>
2023-03-29 23:45:51 +0600

    migration added

---
+++ ExternalLoginApi/ExternalLoginWeb/ExternalLoginWeb/Data/ExternalLoginDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExternalLoginWeb.Data
+{
+    public class ExternalLoginDbContext:DbContext
+    {
+        public ExternalLoginDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
