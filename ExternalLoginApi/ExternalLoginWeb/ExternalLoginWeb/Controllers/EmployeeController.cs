97751ddcdd2000f2474b7809597cf65dd82410f0
Nouf283 <noufawal0311@gmail.com>
2023-03-29 23:47:24 +0600

    Employee Controller added

---
+++ ExternalLoginApi/ExternalLoginWeb/ExternalLoginWeb/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExternalLoginWeb.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
