35707e528b710fe997132a5e1833b88ac62b03c4
Nouf283 <noufawal0311@gmail.com>
2023-03-29 23:56:08 +0600

    Employee controller added:

--- ExternalLoginApi/ExternalLoginWeb/ExternalLoginWeb/Controllers/EmployeeController.cs
+++ ExternalLoginApi/ExternalLoginWeb/ExternalLoginWeb/Controllers/EmployeeController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExternalLoginWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace ExternalLoginWeb.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        private readonly ExternalLoginDbContext _externalLoginDbContext;
+        public EmployeeController(ExternalLoginDbContext externalLoginDbContext)
         {
-            return View();
+            _externalLoginDbContext = externalLoginDbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetEmployees()
+        {
+           var employees= await  _externalLoginDbContext.Employees.ToListAsync();
+            return Ok(employees);
         }
     }
 }
