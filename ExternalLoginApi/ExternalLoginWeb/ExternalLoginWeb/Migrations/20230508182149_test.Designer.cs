df0f559c74c8e03a9670a83a533ef0403fd64966
Nouf283 <noufawal0311@gmail.com>
2023-05-09 00:23:29 +0600

    identity db added

--- ExternalLoginApi/ExternalLoginWeb/ExternalLoginWeb/Migrations/20230508182149_test.Designer.cs
+++ ExternalLoginApi/ExternalLoginWeb/ExternalLoginWeb/Migrations/20230508182149_test.Designer.cs
@@ -4,14 +4,16 @@ using ExternalLoginWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ExternalLoginWeb.Migrations
 {
     [DbContext(typeof(ExternalLoginDbContext))]
-    partial class ExternalLoginDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230508182149_test")]
+    partial class test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
